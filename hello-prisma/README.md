41c09a2aa8b8c8f3150f4172904ce2cabbd1d0a8
Mahdi Hassan <mahdioffi333@gmail.com>
2025-09-18 16:28:55 +0600

    Finding Data from the Database

--- hello-prisma/README.md
+++ hello-prisma/README.md
@@ -2,3 +2,10 @@
 2. migrate - done
 3. generate - done
 4. write query
+
+
+##
+1.create
+2.findMany - array of object
+3.findUnique - return single object
+4.findUniqueOrThrow - return single object if exist, otherwise throw error.
--- hello-prisma/src/index.ts
+++ hello-prisma/src/index.ts
@@ -2,14 +2,40 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main(){
-    const result = await prisma.user.create({
-        data: {
-            name: "abdullah",
-            email: "abdullah@gmail.com"
-        }
-    })
+    // const result = await prisma.user.create({
+    //     data: {
+    //         name: "abdullah",
+    //         email: "abdullah@gmail.com"
+    //     }
+    // })
 
-    console.log(result);
+    // console.log(result);
+
+
+
+    // const userData = await prisma.user.findMany({
+    //     where: {
+    //         id: 3
+    //     }
+    // });
+    // console.log(userData);
+
+
+    // const findUserById = await prisma.user.findUnique({
+    //     where: {
+    //         id: 2
+    //     }
+    // });
+    // console.log(findUserById);
+
+
+
+    const findUserById = await prisma.user.findUniqueOrThrow({
+        where: {
+            id: 5
+        }
+    });
+    console.log(findUserById);
 }
 
 main()
